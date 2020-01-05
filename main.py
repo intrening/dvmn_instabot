5745acbfc9296ed390814db1bb4932acb3f4854a
intrening <wol87@ya.ru>
2020-01-05 15:22:46 +0300

    added set as better solution

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ import argparse
 
 
 def is_user_exist(insta_bot, user_name):
-    if insta_bot.get_user_id_from_username(user_name):
-        return True
-    return False
+    return insta_bot.get_user_id_from_username(user_name)
 
 
 def get_mentioned_users(comment):
@@ -18,6 +16,10 @@ def get_mentioned_users(comment):
 
 
 def is_one_of_users_real(insta_bot, users):
+    #TODO: вопрос, так как у инстабота ограниченные возможности и его очень быстро банят,
+    #я хочу сделать как можно меньше проверок и не проверять, существуют все ли юзеры.
+    #Достаточно проверить, что существует хотя бы один юзер из списка.
+    #тогда я не могу просто вернуть булево значение без if, может, есть какое то решение?
     for user in users:
         if is_user_exist(insta_bot, user):
             return True
@@ -39,18 +41,18 @@ def main():
     insta_bot = Bot()
     insta_bot.login(username=insta_login, password=insta_password)
     media_id = insta_bot.get_media_id_from_link(media_link)
-    media_author_id = insta_bot.get_user_id_from_username(media_author)
-    author_followers = insta_bot.get_user_followers(media_author_id)
     media_comments = insta_bot.get_media_comments_all(media_id)
-    media_likers = insta_bot.get_media_likers(media_id)
+    media_author_id = insta_bot.get_user_id_from_username(media_author)
+    author_followers = set(insta_bot.get_user_followers(media_author_id))
+    media_likers = set(insta_bot.get_media_likers(media_id))
+    appropriate_users = author_followers & media_likers
 
     accepted_comments = []
     for comment in media_comments:
         comment_author_id = str(comment['user_id'])
         mentioned_users = get_mentioned_users(comment)
-        if comment_author_id in media_likers and comment_author_id in author_followers:
-            if is_one_of_users_real(insta_bot=insta_bot, users=mentioned_users):
-                accepted_comments.append(comment)
+        if comment_author_id in appropriate_users and is_one_of_users_real(insta_bot=insta_bot, users=mentioned_users):
+            accepted_comments.append(comment)
 
     accepted_users = set([comment['user_id'] for comment in accepted_comments])
     pprint(accepted_users)
